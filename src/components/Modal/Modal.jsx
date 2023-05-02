1672fc9f0441c2b1716bf32abc5085c2b3ce9178
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-05-02 10:13:35 +0300

    cleared-comments-hw-04-phonebook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -39,35 +39,3 @@ Modal.propTypes = {
   toggleModal: PropTypes.func.isRequired,
   largeImageURL: PropTypes.string.isRequired,
 };
-// export class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handlePressOnEsc);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handlePressOnEsc);
-//   }
-
-//   handlePressOnEsc = event => {
-//     if (event.code === 'Escape') {
-//       this.props.toggleModal();
-//     }
-//   };
-
-//   handleClickOnOverlay = event => {
-//     if (event.currentTarget === event.target) {
-//       this.props.toggleModal();
-//     }
-//   };
-
-//   render() {
-// return createPortal(
-//   <Overlay onClick={this.handleClickOnOverlay}>
-//     <ModalWindow>
-//       <img src={this.props.largeImageURL} alt="#" />
-//     </ModalWindow>
-//   </Overlay>,
-//   modalRoot
-// );
-//   }
-// }
